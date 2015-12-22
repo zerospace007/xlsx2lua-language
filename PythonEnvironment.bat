c724fae27060dcff1ad91eef246c6d0fc6ef92ef
Norman <chuxiang9007@163.com>
2015-12-22 15:07:43 +0800

    csdac

---
+++ PythonEnvironment.bat
@@ -0,0 +1,19 @@
+echo off
+:Start
+
+pip install
+pip install --upgrade
+
+echo ===================================
+echo Pip Install And Upgrade Success
+echo ===================================
+
+pip install xlrd
+
+echo ===================================
+echo Python DownLoad xlrd Execute Success
+echo ===================================
+
+:Exit
+
+pause
---
+++ xlsx2lua-language.bat
@@ -0,0 +1,13 @@
+echo off
+
+:Start
+
+python xlsx2lua-language.py
+
+echo ===================================
+echo xlsx2lua-language Execute Success
+echo ===================================
+
+:Exit
+
+pause
